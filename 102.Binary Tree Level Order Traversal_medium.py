1f80c2e07df989665e60c3432985a87c3a8ed034
jw <wangjia114@gmail.com>
2020-09-13 22:03:13 -0700

    bfs, dfs

---
+++ 102.Binary Tree Level Order Traversal_medium.py
@@ -0,0 +1,56 @@
+# 102. Binary Tree Level Order Traversal
+
+# Solution 1: BFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        res = collections.defaultdict(list)
+
+        depth = 0
+        queue = [(root, depth)]
+
+        while queue:
+            node, depth = queue.pop(0)
+            if node:
+                res[depth].append(node.val)
+                if node.left:
+                    queue.append((node.left, depth + 1))
+                if node.right:
+                    queue.append((node.right, depth + 1))
+
+        return [res[i] for i in res.keys()]
+
+# Solution 2: DFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        res = collections.defaultdict(list)
+        depth = 0
+        stack = [(root, depth)]
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res[depth].append(node.val)
+                if node.right:
+                    stack.append((node.right, depth + 1))
+                if node.left:
+                    stack.append((node.left, depth + 1))
+
+        return [res[i] for i in res.keys()]
