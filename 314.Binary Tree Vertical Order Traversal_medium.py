b0dd55ffc3d1d0061a99339608bd6005c18d9660
jw <wangjia114@gmail.com>
2020-09-14 01:37:48 -0700

    bfs, dfs

--- 314.Binary Tree Vertical Order Traversal_medium.py
+++ 314.Binary Tree Vertical Order Traversal_medium.py
@@ -115,3 +115,33 @@ class Solution:
             v_lists.append(v_list)
 
         return v_lists
+
+# DFS
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        res = collections.defaultdict(list)
+        depth = 0
+        stack = [(root, depth)]
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res[depth].append(node.val)
+                c_node = node.children
+                #注意要从后向前放在stack里
+                c_node = c_node[::-1]
+                for i in c_node:
+                    stack.append((i, depth + 1))
+
+        return [res[i] for i in res.keys()]
---
+++ 429.N-ary Tree Level Order Traversal_medium.py
@@ -0,0 +1,29 @@
+# 429. N-ary Tree Level Order Traversal
+
+# BFS
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if root is None:
+            return []
+
+        res = collections.defaultdict(list)
+
+        depth = 0
+        queue = [(root, depth)]
+
+        while queue:
+            node, depth = queue.pop(0)
+            if node:
+                res[depth].append(node.val)
+                for i in node.children:
+                    if i:
+                        queue.append((i, depth + 1))
+        return [res[i] for i in res.keys()]
