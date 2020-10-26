d0947d1f09f6c302b55a564e0af837032e6162c1
jw <wangjia114@gmail.com>
2020-10-25 17:55:01 -0700

    permutation, 2 pointers

--- 31. Next Permutation_medium.py
+++ 31. Next Permutation_medium.py
@@ -2,12 +2,12 @@
 
 # 解题思路：
 # 双指针
-# 指针[p1]从最后还是走，如果这个数比前面的大，什么都不用做,p1往前走
-# if nums[p1] <= nums[p1-1]: p1 -= 1
-# if nums[p1] > nums[p1-1]:
-# 需要把nums[p1-1]换了
-# 引入p2, 还是从后面开始，找到第一个大于p1-1的值，呼唤
-# 再把p1-1后面的数据排序 —————— 注意不要忘了
+# 指针[p1]从最后还是走，如果这个数比前面的小，什么都不用做,p1往前走， 因为换了不会让number变大
+    # if nums[p1] <= nums[p1-1]: p1 -= 1
+# 如果这个数比前面的大：if nums[p1] > nums[p1-1]:
+    # 需要把nums[p1-1]换了
+    # 引入p2, 还是从后面开始，找到第一个大于p1-1的值，互换
+    # 再把p1-1后面的数据排序 —————— 注意不要忘了
 
 # TC: O(N): In worst case, only two scans of the whole array are needed.
 # SC: O(1)
@@ -16,7 +16,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
         def swap(l, a, b):
             sv = nums[a]
             nums[a] = nums[b]
@@ -25,8 +24,8 @@ class Solution:
         n = len(nums)
         if n <= 1:
             return nums
-        p1 = n - 1
-        p2 = n - 1
+        p1 = n - 1, n - 1
+
         while p1 > 0:
             if nums[p1] > nums[p1 - 1]:
                 while p2 > 0:
@@ -37,6 +36,7 @@ class Solution:
                     p2 -= 1
             p1 -= 1
 
+        # 最后如果p2如果没有变，说明原来的数是最大的，就返回一个最小数
         if p2 == n - 1:
             return nums.sort()
 
